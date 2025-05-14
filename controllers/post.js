d130f0a3241fa9fe0559818b3e7be3c909c49550
Rankush <iamrankush@gmail.com>
2025-05-15 00:29:17 +0530

    more routes created

--- controllers/post.js
+++ controllers/post.js
@@ -10,6 +10,48 @@ const post = async (req, res) => {
   }
 };
 
+const getPosts = async (req, res) => {
+  try {
+    const data = await Post.find({});
+
+    res.json({ data: data });
+  } catch (error) {
+    res.status(500).json({ message: "Error in fetching post details", error });
+  }
+};
+
+const updatePost = async (req, res) => {
+  try {
+    const { title, description, author } = req.body;
+    const id = req.params.id;
+
+    const found = await Post.findById(id);
+    if (!found) {
+      return res.status(404).json({ message: "Post not found" });
+    }
+
+    const data = await Post.updateOne(
+      { _id: id },
+      { $set: { title, description, author } }
+    );
+
+    res.json({ data: data });
+  } catch (error) {
+    res.status(500).json({ message: "Error in updating post details", error });
+  }
+};
+
+const deletePost = async (req, res) => {
+  try {
+    const id = req.params.id;
+    const data = await Post.deleteOne({ _id: id });
+
+    res.json({ data: data, message: "Post deleted successfully" });
+  } catch (error) {
+    res.status(500).json({ message: "Error in deleting post details", error });
+  }
+};
+
 const getPostDetails_withUserInfo = async (req, res) => {
   try {
     const data = await Post.aggregate([
@@ -29,4 +71,10 @@ const getPostDetails_withUserInfo = async (req, res) => {
   }
 };
 
-module.exports = { post, getPostDetails_withUserInfo };
+module.exports = {
+  post,
+  getPostDetails_withUserInfo,
+  getPosts,
+  updatePost,
+  deletePost,
+};
--- routes/post.ts
+++ routes/post.ts
@@ -1,8 +1,19 @@
 const express = require("express");
 const router = express.Router();
-const { post, getPostDetails_withUserInfo } = require("../controllers/post");
+const {
+  post,
+  getPostDetails_withUserInfo,
+  getPosts,
+  updatePost,
+  deletePost,
+} = require("../controllers/post");
 
 router.post("/", post);
 router.get("/getPostDetails", getPostDetails_withUserInfo);
+
+router.get("/getPosts", getPosts);
+router.post("/updatePost/:id", updatePost);
+
+router.delete("/deletePost/:id", deletePost);
 
 module.exports = router;
